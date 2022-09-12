0a027eee317285131329a66606440288b7a98a2d
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-09-12 17:33:23 +0200

    test2

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include('config/fonctions.php');
+include('code/config/fonctions.php');
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +12,8 @@ include('config/fonctions.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Pascal Vanmalderen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="./style.css">
-    <link rel="stylesheet" href="./responsive.css">
+    <link rel="stylesheet" href="./code/style.css">
+    <link rel="stylesheet" href="./code/responsive.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
