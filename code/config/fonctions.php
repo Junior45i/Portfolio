46f292d755aa73fadd2f341b20a4f037fde9294b
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-09-12 22:53:12 +0200

    essai DB

--- code/config/fonctions.php
+++ code/config/fonctions.php
@@ -1,5 +1,5 @@
 <?php
 // Connection à la db
-include 'config/database.php';
+include 'database.php';
 $conn = connectDB("https://pascalvanmalderen.fr/", "u933945483_projet", "u933945483_root", "Pascal2244");
 ?>
