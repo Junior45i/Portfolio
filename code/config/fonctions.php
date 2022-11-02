042a97854532e42038e2f2e73b507f7435f18cdd
Pascal Vanmalderen <pascal.vanmalderen@hotmail.be>
2022-11-02 17:53:02 +0100

    test

--- code/config/fonctions.php
+++ code/config/fonctions.php
@@ -1,6 +1,5 @@
 <?php
-// Connection à la db coucou
-
+// Connection à la db coucoux2
 include 'database.php';
 $conn = connectDB("localhost", "u933945483_projet", "u933945483_root", "Pascal2244");
 ?>
